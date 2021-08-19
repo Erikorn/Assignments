5c583e684b07fd65434239e9d326d4f9d520ade2
Erik Tycksen <eriktycksen@gmail.com>
2021-08-18 18:14:21 -0600

    Game Over Function Works!

--- Level 1/Escape Room/main.js
+++ Level 1/Escape Room/main.js
@@ -2,80 +2,136 @@ const readlineSync = require("readline-sync");
 
       let characterAlive = true;
       let doorClosed = true;
-      let doesNotHaveKey = true;
+      let foundDoorKey = false;
+      let knockedOnDoor = false;
       let triedToOpenDoor = false;
+      let lookedThroughHole = false;
+      let searchedBucket = false;
 
-console.log ("\nYou find yourself waking up in a dark room.  \nA small, barred hole in the ceiling reveals just enough moonlight for you to make out three things: \A door, a hole in the wall, and a bucket.")
+console.log ("\nYou wake in a cold and dark room.  \nA small, barred hole in the ceiling reveals \njust enough moonlight for you to make out three things: \nA door, a hole in the wall, and a bucket.")
 
 function escapeRoom() {
   let escapeRoom = readlineSync.keyIn("\nWhat do you do? \n\n[a]Inspect the Door \n[b]Inspect the Hole \n[c]Inspect the Bucket \n\n", {limit: ("abc")})
+
   if (escapeRoom === "a") {
-    console.log ("\nIt appears to be a large and solid wooden door, \nwith a smaller trap door in the middle.  \nThere is also an iron keyhole on the side.")  
+    console.log ("\nIt appears to be a large and solid wooden door, \nwith a smaller wicket door in the center.  \nThere is also an iron keyhole on the side.")  
   }
-  while (escapeRoom === "a")
-  {let inspectDoor = readlineSync.keyIn(" \n[a]Knock on the Door \n[b]Open the Door \n[c]Leave the Door Alone \n\n", {limit: ("abc")})
+
+  while (escapeRoom === "a") {
+    let inspectDoor = readlineSync.keyIn(" \n[a]Knock on the Door \n[b]Open the Door \n[c]Leave the Door Alone \n\n", {limit: ("abc")})
+
     if (inspectDoor === "a") {
-      console.log("\nYou pound on the door with your first, \nas soon as you finish the small trap door flings open and a Dwarf appears, \npunching you in the groin.  As you collapse to the floor, the tiny door slams shut, \na chorus of gruff laughter erupting from the other side. \n\nWASTED\n")
-      characterAlive = false;
-  break
+      if (knockedOnDoor === false) {
+      console.log("\nAlmost as soon as your fist connects with the door, \nan angry looking dwarf opens the wicket door and punches you in the groin.  \nThe tiny door slams shut as you collapse to the floor. \nA chorus of gruff laughter erupting from the other side.")
+      knockedOnDoor = true;
+      }
+
+      else {
+        console.log ("\nYeah no, I don't think that's a good idea.")
+      }
   }
+
     else if (inspectDoor === "b") {
-      if (doesNotHaveKey === false) {
-        console.log("\nYou insert the horrible smelling key into the iron lock, \nwith some effort it turns with a loud click.  \nThe door swings opens easily, exposing a line of grinning Dwarves, \ntheir palms raised in the air, waiting to reward you with high fives in celebration of your glorious victory.\n\nThe End\n")
+      if (foundDoorKey === true) {
+        console.log("\nYou insert the foul-smelling key into the iron lock. \nWith some effort it turns with a loud click.  \nThe door swings opens easily, exposing a line of grinning Dwarves. \nTheir palms raised in the air, waiting to reward you with high fives in celebration of your glorious victory.\n\nThe End\n")
         doorClosed = false;
         break
       }
+
       else if (triedToOpenDoor === false) {
-        console.log("\nYou try the door, it does not budge.  \nYou hear smirking from a group of people on the other side. ")
+        console.log("\nYou try the door.  It seems to be locked and will not budge.  \nYou hear a lot of smirks and chuckles from the other side. ")
         triedToOpenDoor = true;
       } 
+
       else {
-        console.log("You already tried that idiot.")
+        console.log("\nYou already tried that idiot. \nIt's obviously still locked.")
       }     
     }
+
     else if (inspectDoor === "c") {
       console.log("\nYou step back, leaving the door alone for now. ")
     break
     }
   }
+
   if (escapeRoom === "b") {
-    console.log ("\nIt appears to be a small, dark and mysterious hole in the wall with a very ominous feeling.  \nIt is roughly wide enough to put your hand in. ")
+    console.log ("\nYou step forward, taking a closer look at the hole. \nYour gaze transfixed by the ominous darkness inside.  \nIt seems to be just about wide enough to reach through. ")
   }
+
   while (escapeRoom === "b") {
-    let inspectHole = readlineSync.keyIn("\n[a]Look through the Hole \n[b]Reach into the Hole \n[c]Leave the Hole Alone \n\n", {limit: ("abc")})
+    let inspectHole = readlineSync.keyIn("\n[a]Peek through the Frightening Hole \n[b]Reach into the Terrifying Hole \n[c]Leave the Scary-Ass Hole Alone \n\n", {limit: ("abc")})
+
     if (inspectHole === "a") {
-      console.log("\nYou crouch down to peer through the hole, as soon as you're eye level lines up, \na solid green fist pops out and punches you in the face. ")
-    }
-    else if (inspectHole === "b") {
-      console.log("\nTentatively, you reach your hand into the hole.  \nA very solid and strong green hand locks around your wrist and yanks with an insane amount of force, \nripping your arm out of it's socket.  \nAs you fall to the ground, your life force seeping from you, you hear a sadistic giggling.  \nFor some reason, you feel like this was not a smart thing to do. \n\nWASTED\n")
-      characterAlive = false;
-      break
-    }
-    else if (inspectHole === "c") {
-      console.log("\nYou leave the scary hole alone, \nfeeling like you have avoided something terrible. ")
-      break
-    }
-  }
-  if (escapeRoom === "c") {
-    console.log ("\nIt appears to be a simple metal bucket, \nit smells horrible...  ")
-  }
-  while (escapeRoom === "c") {
-    let inspectBucket = readlineSync.keyIn("\n[a]Kick the Bucket \n[b]Search the Bucket\n[c]Leave the Bucket Alone \n\n", {limit: ("abc")})
-    if (inspectBucket === "a") {
-      let kickBucket = readlineSync.keyIn("\nThis bucket smells horrible as it is! \nAre you sure you want to spread it's contents all over the floor? \n\n[a]On second thought, better not. \n[b]KICK IT!!!!! \n\n", {limit: ("ab")})
-      if (kickBucket === "a") {
-        console.log("\nYou leave the smelly bucket alone for now. ")
+      if (lookedThroughHole === false) {
+      console.log("\nAs you crouch down to peer through the hole, \na very green and VERY solid fist punches you in the face. ")
+      lookedThroughHole = true;
       }
-      else if (kickBucket === "b") {
-        console.log("\nThe sound of your foot connecting with the bucket rings loudly throughout the hollow room. \nThe vile contents fly through the air, splattering the walls and door.  \nYou hear loud footsteps ring down the hallway, coming closer until the door bangs open. \nA stout and smelly Dwarf pulls out a sword and runs you through the stomach.  \nAs you collapse to the floor, your innards spilling out, you have one last ironic though: \nNow you have kicked the bucket both figuratively and literally. \n\nWASTED\n")
+
+      else {
+        console.log ("\nAgainst your better judgement, \nyou crouch back down to look through the hole again.  \nOnce again youb find yourself on your back, this time with a broken nose.  \nI don't know why you thought that was a good idea. ")
+      }
+    }
+
+    else if (inspectHole === "b") {
+      console.log("\nThe voice of reason in your head screams for you to stop. \nYou tentatively reach your hand into the hole.  \nA green hand suddenly locks around your wrist with a terrifying strength. \nPulling with an insane amount of force, you hear the sickening \nsound of flesh tearing as your arm is ripped out of it's socket.  \nYou fall to the ground, life force gushing from where your shoulder used to be.  \nAs your vision fades, a sadistic cackling can be heard from the other side.  \nFor some reason, you feel like this was not a very smart thing to do. \n\nYou dead.\n")
+      let gameOver = readlineSync.keyIn("\nWould you like to play again? \n\n[a]Yes [b]No\n\n")
+
+      if (gameOver === "a") {
+        console.log ("\nYou wake in a cold and dark room.  \nA small, barred hole in the ceiling reveals \njust enough moonlight for you to make out three things: \nA door, a hole in the wall, and a bucket.")
+        characterAlive = true; 
+        doorClosed = true; 
+        foundDoorKey = false; 
+        knockedOnDoor = false; 
+        triedToOpenDoor = false; 
+        lookedThroughHole = false; 
+        searchedBucket = false; 
+        break
+      }
+      else if (gameOver === "b") {
+        console.log("Goodbye") 
         characterAlive = false;
         break
       }
     }
-    else if (inspectBucket === "b") {
-      console.log("\nWhile holding your breath, you reach your hand into the Bucket.  \nTrying not to think of what you're sifting through, you grasp onto something cool and hard.  \nPulling your hand out, you find yourself holding a very dirty and smelly iron key. ")
-      doesNotHaveKey = false;
+
+    else if (inspectHole === "c") {
+    console.log("\nYou manage to tear your gaze away and leave the Scary-Ass Hole Alone, \nRelief washes over you, and you feel as if you avoided something terrible. ")
+    break
     }
+  }
+  if (escapeRoom === "c") {
+    console.log ("\nA fetid stench invades your senses as you approach.  \nIt appears to be a simple metal bucket... \nA very smelly, simple, metal bucket.")
+  }
+
+  while (escapeRoom === "c") {
+    let inspectBucket = readlineSync.keyIn("\n[a]Kick the Stinky Bucket! \n[b]Search the Smelly Bucket.\n[c]Leave the Foul Smelling-Bucket Alone. \n\n", {limit: ("abc")})
+    
+    if (inspectBucket === "a") {
+      let kickBucket = readlineSync.keyIn("\nThis bucket smells foul enough as it is! \nAre you sure you want its contents spread across the room? \n\n[a]KICK IT!!!!! \n[b]On second thought, better not. \n\n", {limit: ("ab")})
+      
+      if (kickBucket === "a") {
+        console.log("\nWith a satisfying punt, you send the bucket soaring across the room.  \nIt's vile filth explodes throughout the room as it crashes against the wall. \nYou hear short footsteps angrily race toward your cell. \nThe door bursting open, a stout and angry Dwarf unsheathes a short sword and runs you through.  \nYou Collapse to the floor, desperately clinging to your stomach \nin vain as your innards spill out.  As you perish, your final, dying thought is an ironic one: \nYou just figuratively and literally kicked the bucket. \n\nYou dead, fool.\n")
+        characterAlive = false;
+        break
+      }
+
+      else if (kickBucket === "b") {
+        console.log("\nYou leave the smelly bucket alone for now. ")
+      }
+    }
+
+    else if (inspectBucket === "b") {
+      if (searchedBucket === false) {
+        console.log("\nYou try to suppress a gag, holding your breath as you reach into the Bucket.  \nTrying not to think of what you're sifting through, you hand clasps onto something cool and hard.  \nPulling your hand out, you find yourself holding a very dirty, and smelly iron key. ")
+        searchedBucket = true;
+        foundDoorKey = true;
+      }
+
+      else {
+        console.log("What is wrong with you?")
+      }
+    }
+
     else if (inspectBucket === "c") {
       console.log("\nYou leave the smelly bucket alone for now. ")
       break
@@ -86,11 +142,3 @@ function escapeRoom() {
 while (characterAlive && doorClosed) {
   escapeRoom()
 }
-
-
-
-/*
-Want to add the option to play again at the end of every game.
-Want to have the option for multiple responses to the same answer (look in the hole 1, look in the hole 2 search the bucket 1 search the bucket 2)
-Proof read and polish
-*/
